342107dd093dcc8781a29a84e1809ab3524ce334
mavisoft <iyoboiyoha@gmail.com>
2022-09-09 09:07:45 -0700

    fix task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,13 +12,13 @@ int main(void)
 
         for (a = 48; a <=57; a++)
         {
-                for (b = 49; b <= 58; b++)
+                for (b = 49; b <= 57; b++)
                 {
                         if ( a != b && b > a)
                         {
                                 putchar(a);
                                 putchar(b);
-                                putchar(', ');
+                                putchar(',');
                         }
                 }
         }
