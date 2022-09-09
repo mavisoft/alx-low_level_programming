ce487fb55a83f445c2d6688ce6197e2e8a478d3e
mavisoft <iyoboiyoha@gmail.com>
2022-09-09 09:00:11 -0700

    task 10 commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,7 @@ int main(void)
 
         for (a = 48; a <=57; a++)
         {
-                for (b = 49; b <= 57; b++)
+                for (b = 49; b <= 58; b++)
                 {
                         if ( a != b && b > a)
                         {
