77f56ce165b91b595ec755c07453e4d6c9f262cb
mavisoft <iyoboiyoha@gmail.com>
2022-09-09 07:54:27 -0700

    task 5 commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: mavis
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int mav;
+
+	for (mav = 0; mav < 10; mav++)
+	{
+		printf("%d", mav);
+	}
+	printf("\n");
+	return (0);
+}
