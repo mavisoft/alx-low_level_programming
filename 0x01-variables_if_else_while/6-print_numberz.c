033fab92e79ec233f6392bcd2974c49f5fd5b9ea
mavisoft <iyoboiyoha@gmail.com>
2022-09-09 08:07:51 -0700

    task 6 commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: mavis
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int mav;
+
+	for (mav = 0; mav < 10; mav++)
+		putchar(mav);
+	putchar('\n');
+	return (0);
+}
