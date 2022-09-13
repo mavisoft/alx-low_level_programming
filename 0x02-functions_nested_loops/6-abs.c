2ee7f3ef881bff3b580cfac14798452a21f7dc45
mavisoft <iyoboiyoha@gmail.com>
2022-09-13 12:58:20 -0700

    task 6 commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - computes absolute value of a value
+ * @c: integer
+ * description : computes integer
+ *
+ * Return: int
+ */
+int _abs(int c)
+{
+	if (c > 0)
+	{
+		c = +c;
+	}
+	else
+	{
+		c = -c;
+	}
+	return (c);
+}
