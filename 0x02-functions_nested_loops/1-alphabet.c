280956fc67dc03242770de0af6060a14800875f5
mavisoft <iyoboiyoha@gmail.com>
2022-09-13 10:59:36 -0700

    task 1 commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description - a function that prints the alphabet, in lowercase, followed by a new line
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	char mavis;
+
+	for (mavis = 'a'; mavis <= 'z'; mavis++)
+	{
+		_putchar(mavis);
+	}
+	_putchar('\n');
+	return (0);
+}
