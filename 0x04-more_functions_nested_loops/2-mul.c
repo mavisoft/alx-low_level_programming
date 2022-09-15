8de51dee529f84be07f38755d65cb3e3fd63d1b4
mavisoft <iyoboiyoha@gmail.com>
2022-09-15 06:17:48 -0700

    task 2 commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *a function that multiplies two integers
+ * Return - x
+ */
+
+int mul(int a, int b)
+{
+	x = a * b;
+
+	printf("Answer is equals %if" a, b, x);
+
+	return(x);
+}
