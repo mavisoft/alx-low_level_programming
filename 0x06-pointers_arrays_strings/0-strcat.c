00f225ed45dfa0ed126a33ee0408fa595bc9c83d
mavisoft <iyoboiyoha@gmail.com>
2022-09-22 00:56:10 -0700

    task 0 commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,13 +4,12 @@
  * _strcat - concatenate two strings
  * @dest: char string to concatenate to
  * @src: char string
- * Return: pointer to resulting string `dest`
+ * Return: pointer to dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	char dest[] = "Hello ";
-	char src[] = "World!";
-	printf("Concatenated String: %s\n",  _strcat(dest,src));
-	return 0;
+	strcat(dest, src);
+
+	return (dest);
 }
