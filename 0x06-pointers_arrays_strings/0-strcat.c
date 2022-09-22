fcaff5481249d2aa0c2edc9c4a1b1db1deaa60ea
mavisoft <iyoboiyoha@gmail.com>
2022-09-22 01:05:32 -0700

    task 0 commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,25 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strcat - concatenate two strings
  * @dest: char string to concatenate to
  * @src: char string
- * Return: pointer to dest
+ * Return: pointer to resulting string `dest`
  */
 
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
+	int i, c;
 
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (c = 0; src[c] != '\0'; c++)
+	{
+		dest[i] = src[c];
+		i++;
+	}
+
+	dest[i] = '\0';
 	return (dest);
 }
