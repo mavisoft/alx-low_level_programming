0ec79c4c7a03888b535d38743a4c2792e771de1a
mavisoft <iyoboiyoha@gmail.com>
2022-09-30 20:59:30 -0700

    task 1 commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the number of arguments passed to it.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return always 0
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("= %d", argc);
+
+	return 0;
+}
